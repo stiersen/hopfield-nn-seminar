80f90a7c025cd89c46ff78a79de2b4ebdc2965b7
Timo k <toger5@hotmail.de>
2021-05-28 19:26:38 +0200

    add simple plotting with update button

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import hopfield
 from hopfield import Hopfield
 import numpy as np
-
+import matplotlib.pyplot as plt
+from matplotlib.widgets import Button 
 h = Hopfield(3)
 cross = np.array([[1,-1,1],
 [-1,1,-1],
@@ -14,9 +15,22 @@ h.add_shape(cross)
 h.train_all_shapes()
 start = np.array([[-1,1,-1],
 [1,-1,1],
-[-1,1,-1]])
+[1,1,-1]])
+
 h.set_network(start)
-while True:
+
+def plt_do_iteration(event):
     hopfield.asci_print(h.network)
     h.update()
-    input()
+    plt.imshow(h.network)
+    plt.draw()
+
+plt.imshow(h.network)
+axprev = plt.axes()
+# axprev = plt.axes([0.5, 0, 0.1, 0.075])
+bnext = Button(axprev, 'Do Iteration')
+bnext.on_clicked(plt_do_iteration)
+# bprev = Button('Previous')
+# bprev.on_clicked(callback.prev)
+
+plt.show()
